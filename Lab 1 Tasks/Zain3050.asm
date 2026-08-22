61308cb7bdaee13b5e9b170bca61cd3a38831ad3
Muhammad Zain <k253050@nu.edu.pk>
2026-08-22 15:31:50 +0500

    Update Zain3050.asm

--- Lab 1 Tasks/Zain3050.asm
+++ Lab 1 Tasks/Zain3050.asm
@@ -1,9 +1,13 @@
 INCLUDE Irvine32.inc
 .data
 message BYTE "Hello, I'm Student of Your class, MUhammad Zain, having roll number 25K-3050, Of BSE-3A", 0
+message1 BYTE "Proud to be A part of FAST Campus", 0
 .code
 main PROC
 mov edx, OFFSET message
+call WriteString
+call Crlf
+mov edx, OFFSET message1
 call WriteString
 call Crlf
 exit
